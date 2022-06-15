68d0d4b37802923777e606f783a60cd58ccd68d3
Tarun <tarsuno@gmail.com>
2022-06-15 11:40:49 +0200

    Fix endpoint

--- launcher.sh
+++ launcher.sh
@@ -5,8 +5,7 @@ CLI_NAME="state-analyzer"
 echo "launching State-Analyzer"
 
 
-BN_ENDPOINT="https://20PdJoS82pnejJJ9joDMnbjsQ32:0c9b868d8621332ea91c7fc24c5fc34f@eth2-beacon-mainnet.infura.io"
-#BN_ENDPOINT="http://localhost:5052"
+BN_ENDPOINT="http://localhost:5052"
 OUT_FOLDER="results"
 INIT_SLOT="300000"
 FINAL_SLOT="300063"
