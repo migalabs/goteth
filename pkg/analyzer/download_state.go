26398e87f65a002df95f634fc6afadab8bd39f70
Tarun <tarsuno@gmail.com>
2022-09-05 11:56:17 +0200

    Added go routine to download finalized state in parallel

--- pkg/analyzer/download_state.go
+++ pkg/analyzer/download_state.go
@@ -63,3 +63,71 @@ func (s *StateAnalyzer) runDownloadStates(wgDownload *sync.WaitGroup) {
 
 	log.Infof("All states for the slot ranges has been successfully retrieved, clossing go routine")
 }
+
+func (s *StateAnalyzer) runDownloadStatesFinalized(wgDownload *sync.WaitGroup) {
+	defer wgDownload.Done()
+	log.Info("Launching Beacon State Finalized Requester")
+	// loop over the list of slots that we need to analyze
+	var prevBState spec.VersionedBeaconState // to be checked, it may make calculation easier to store previous state
+	var bstate *spec.VersionedBeaconState
+	finalizedSlot := 0
+	timerCh := time.Tick(time.Second * 12)
+	for {
+		ticker := time.NewTicker(minReqTime)
+		select {
+		case <-s.ctx.Done():
+			log.Info("context has died, closing state requester routine")
+			close(s.EpochTaskChan)
+			return
+		case <-timerCh:
+
+			firstIteration := false
+			// make the state query
+			log.Infof("requesting Beacon State from endpoint: finalized")
+			if bstate != nil { // in case we already had a bstate (only false the first time)
+				prevBState = *bstate
+			} else {
+				firstIteration = true
+			}
+			snapshot := time.Now()
+			header, err := s.cli.Api.BeaconBlockHeader(s.ctx, "finalized")
+			if err != nil {
+				log.Errorf("Unable to retrieve Beacon State from the beacon node, closing finalized requester routine. %s", err.Error())
+				return
+			}
+			if int(header.Header.Message.Slot) == finalizedSlot {
+				log.Infof("No new finalized state yet")
+				continue
+			}
+			finalizedSlot = int(header.Header.Message.Slot)
+			bstate, err = s.cli.Api.BeaconState(s.ctx, fmt.Sprintf("%d", finalizedSlot))
+			s.MonitorMetrics.AddDownload(time.Since(snapshot).Seconds())
+			if !firstIteration {
+				// only execute tasks if it is not the first iteration
+				if err != nil {
+					// close the channel (to tell other routines to stop processing and end)
+					log.Errorf("Unable to retrieve Beacon State from the beacon node, closing requester routine. %s", err.Error())
+					return
+				}
+
+				// we now only compose one single task that contains a list of validator indexes
+				// compose the next task
+				epochTask := &EpochTask{
+					ValIdxs:   s.ValidatorIndexes,
+					Slot:      uint64(finalizedSlot),
+					State:     bstate,
+					PrevState: prevBState,
+				}
+
+				log.Debugf("sending task for slot: %d", finalizedSlot)
+				s.EpochTaskChan <- epochTask
+			}
+		default:
+
+		}
+		// check if the min Request time has been completed (to avoid spaming the API)
+		<-ticker.C
+	}
+
+	// log.Infof("All states for the slot ranges has been successfully retrieved, clossing go routine")
+}
