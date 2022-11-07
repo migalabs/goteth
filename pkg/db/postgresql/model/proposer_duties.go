9407402abb7363b1ff40dabec255cf9ad6916439
Tarun <tarsuno@gmail.com>
2022-11-07 10:38:57 +0100

    Add missed blocks to proposer duties table

--- pkg/db/postgresql/model/proposer_duties.go
+++ pkg/db/postgresql/model/proposer_duties.go
@@ -6,13 +6,15 @@ var (
 	CREATE TABLE IF NOT EXISTS t_proposer_duties(
 		f_val_idx INT,
 		f_proposer_slot INT,
+		f_proposed BOOL,
 		CONSTRAINT PK_Val_Slot PRIMARY KEY (f_val_idx, f_proposer_slot));`
 
 	InsertProposerDuty = `
 	INSERT INTO t_proposer_duties (
 		f_val_idx, 
-		f_proposer_slot)
-		VALUES ($1, $2)
+		f_proposer_slot,
+		f_proposed)
+		VALUES ($1, $2, $3)
 		ON CONFLICT DO NOTHING;
 	`
 	// if there is a confilct the line already exists
@@ -21,15 +23,18 @@ var (
 type ProposerDuties struct {
 	ValIdx       uint64
 	ProposerSlot uint64
+	Proposed     bool
 }
 
 func NewProposerDuties(
 	iValIdx uint64,
-	iProposerSlot uint64) ProposerDuties {
+	iProposerSlot uint64,
+	iProposed bool) ProposerDuties {
 
 	return ProposerDuties{
 		ValIdx:       iValIdx,
 		ProposerSlot: iProposerSlot,
+		Proposed:     iProposed,
 	}
 }
 
