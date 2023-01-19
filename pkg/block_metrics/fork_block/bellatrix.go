a68c7e40e5ac8db1350d1db81d885d5d5919a723
Tarun <tarsuno@gmail.com>
2023-01-19 15:57:06 +0100

    Adapt constructors to new standard block

--- pkg/block_metrics/fork_block/bellatrix.go
+++ pkg/block_metrics/fork_block/bellatrix.go
@@ -1,13 +1,28 @@
 package fork_block
 
-import "github.com/attestantio/go-eth2-client/spec"
+import (
+	"github.com/attestantio/go-eth2-client/spec"
+)
 
 func NewBellatrixBlock(block spec.VersionedSignedBeaconBlock) ForkBlockContentBase {
 	return ForkBlockContentBase{
-		Slot:          uint64(block.Bellatrix.Message.Slot),
-		ProposerIndex: uint64(block.Bellatrix.Message.ProposerIndex),
-		Graffiti:      block.Bellatrix.Message.Body.Graffiti,
-		Attestations:  block.Bellatrix.Message.Body.Attestations,
-		Deposits:      block.Bellatrix.Message.Body.Deposits,
+		Slot:              uint64(block.Bellatrix.Message.Slot),
+		ProposerIndex:     uint64(block.Bellatrix.Message.ProposerIndex),
+		Graffiti:          block.Bellatrix.Message.Body.Graffiti,
+		Attestations:      block.Bellatrix.Message.Body.Attestations,
+		Deposits:          block.Bellatrix.Message.Body.Deposits,
+		ProposerSlashings: block.Bellatrix.Message.Body.ProposerSlashings,
+		AttesterSlashings: block.Bellatrix.Message.Body.AttesterSlashings,
+		VoluntaryExits:    block.Bellatrix.Message.Body.VoluntaryExits,
+		SyncAggregate:     block.Bellatrix.Message.Body.SyncAggregate,
+		ExecutionPayload: ForkBlockPayloadBase{
+			FeeRecipient:  block.Bellatrix.Message.Body.ExecutionPayload.FeeRecipient,
+			GasLimit:      block.Bellatrix.Message.Body.ExecutionPayload.GasLimit,
+			GasUsed:       block.Bellatrix.Message.Body.ExecutionPayload.GasUsed,
+			Timestamp:     block.Bellatrix.Message.Body.ExecutionPayload.Timestamp,
+			BaseFeePerGas: block.Bellatrix.Message.Body.ExecutionPayload.BaseFeePerGas,
+			BlockHash:     block.Bellatrix.Message.Body.ExecutionPayload.BlockHash,
+			Transactions:  block.Bellatrix.Message.Body.ExecutionPayload.Transactions,
+		},
 	}
 }
