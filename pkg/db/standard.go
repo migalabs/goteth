9879c0691a1af6f20e6f294efb7af384c83fae8f
Tarun <tarsuno@gmail.com>
2023-04-18 12:42:43 +0200

    Remove extra interface

--- pkg/db/standard.go
+++ pkg/db/standard.go
@@ -2,15 +2,6 @@ package db
 
 import "github.com/cortze/eth-cl-state-analyzer/pkg/db/model"
 
-// Every database service must implement the following
-
-type DatabaseService interface {
-	init()
-	Close()
-	runWriters()
-	Persist()
-}
-
 type WriteTask struct {
 	Model model.Model
 	Op    string
