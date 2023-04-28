5d615a618212101dc8e176fefb0a710ff2eb7e48
ocheja <ocheja.patrick@gmail.com>
2023-04-28 07:45:30 +0200

    create transactions table

--- pkg/db/service.go
+++ pkg/db/service.go
@@ -124,6 +124,11 @@ func (p *PostgresDBService) init(ctx context.Context, pool *pgxpool.Pool) error 
 		return err
 	}
 
+	err = p.createTransactionsTable()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
