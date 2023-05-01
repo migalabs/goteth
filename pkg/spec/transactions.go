29cd1775d74630643c1868445ca95062269786d2
ocheja <ocheja.patrick@gmail.com>
2023-05-01 15:46:40 +0200

    implement transactions spec with agnostic transaction type and request transaction details to be processed

---
+++ pkg/spec/transactions.go
@@ -0,0 +1,54 @@
+package spec
+
+import (
+	"encoding/hex"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// A wrapper for blockchain transaction with basic information retrieved from the Ethereum blockchain
+type AgnosticTransaction struct {
+	TxType    uint8
+	ChainId   uint64
+	Data      string
+	Gas       uint64
+	GasPrice  uint64
+	GasTipCap uint64
+	GasFeeCap uint64
+	Value     uint64
+	Nonce     uint64
+	To        *common.Address
+	Hash      common.Hash
+	Size      uint64
+}
+
+func (txs AgnosticTransaction) Type() ModelType {
+	return TransactionsModel
+}
+
+// convert transactions from byte sequences to Transaction object
+func RequestTransactionDetails(block AgnosticBlock) []*AgnosticTransaction {
+	processedTransactions := make([]*AgnosticTransaction, 0)
+	for idx := 0; idx < len(block.ExecutionPayload.Transactions); idx++ {
+		var parsedTx = &types.Transaction{}
+		if err := parsedTx.UnmarshalBinary(block.ExecutionPayload.Transactions[idx]); err != nil {
+			return nil
+		}
+
+		processedTransactions = append(processedTransactions, &AgnosticTransaction{
+			TxType:    parsedTx.Type(),
+			ChainId:   parsedTx.ChainId().Uint64(),
+			Data:      hex.EncodeToString(parsedTx.Data()),
+			Gas:       parsedTx.Gas(),
+			GasPrice:  parsedTx.GasPrice().Uint64(),
+			GasTipCap: parsedTx.GasTipCap().Uint64(),
+			GasFeeCap: parsedTx.GasFeeCap().Uint64(),
+			Value:     parsedTx.Value().Uint64(),
+			Nonce:     parsedTx.Nonce(),
+			To:        parsedTx.To(),
+			Hash:      parsedTx.Hash(),
+			Size:      parsedTx.Size(),
+		})
+	}
+	return processedTransactions
+}
