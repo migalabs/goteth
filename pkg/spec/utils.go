2fbfe5bb96b45d9825c00219961656f540ce60ad
Tarun <tarsuno@gmail.com>
2023-05-23 12:52:08 +0200

    Move generic function to utils

--- pkg/spec/utils.go
+++ pkg/spec/utils.go
@@ -1,9 +1,20 @@
 package spec
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/sirupsen/logrus"
+)
 
 var (
 	log = logrus.WithField(
 		"module", "spec",
 	)
 )
+
+func IsActive(validator phase0.Validator, epoch phase0.Epoch) bool {
+	if validator.ActivationEpoch <= epoch &&
+		epoch < validator.ExitEpoch {
+		return true
+	}
+	return false
+}
