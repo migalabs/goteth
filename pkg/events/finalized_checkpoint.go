ceae196cdf63306874af26398303e8ffc599eb26
Tarun <tarsuno@gmail.com>
2023-06-21 16:58:39 +0200

    Add finalized subscription and channel, also only send through channel when possible

---
+++ pkg/events/finalized_checkpoint.go
@@ -0,0 +1,27 @@
+package events
+
+import api "github.com/attestantio/go-eth2-client/api/v1"
+
+func (e *Events) SubscribeToFinalizedCheckpointEvents() {
+	// subscribe to head event
+	err := e.cli.Api.Events(e.ctx, []string{"finalized_checkpoint"}, e.HandleCheckpointEvent) // every new head
+	if err != nil {
+		log.Panicf("failed to subscribe to head events: %s", err)
+	}
+	log.Infof("subscribed to finalized checkpoint events")
+}
+
+func (e *Events) HandleCheckpointEvent(event *api.Event) {
+	log := log.WithField("routine", "checkpoint-event")
+	if event.Data == nil {
+		return
+	}
+
+	data := event.Data.(*api.FinalizedCheckpointEvent) // cast to head event
+	log.Infof("Received a new event: epoch %d, slot root: %s", data.Epoch, data.State.String())
+
+	select { // only notify if we can
+	case e.FinalizedChan <- *data:
+	default:
+	}
+}
