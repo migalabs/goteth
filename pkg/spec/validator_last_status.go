b8fda0e53dfd0fb1b1b10a296057fd26de08425d
Tarun <tarsuno@gmail.com>
2023-07-13 10:05:41 +0200

    Add data structures to persist more columns into validator_last_status

--- pkg/spec/validator_last_status.go
+++ pkg/spec/validator_last_status.go
@@ -5,10 +5,15 @@ import (
 )
 
 type ValidatorLastStatus struct {
-	ValIdx         phase0.ValidatorIndex
-	Epoch          phase0.Epoch
-	CurrentBalance phase0.Gwei
-	CurrentStatus  ValidatorStatus
+	ValIdx          phase0.ValidatorIndex
+	Epoch           phase0.Epoch
+	CurrentBalance  phase0.Gwei
+	CurrentStatus   ValidatorStatus
+	Slashed         bool
+	ActivationEpoch phase0.Epoch
+	WithdrawalEpoch phase0.Epoch
+	ExitEpoch       phase0.Epoch
+	PublicKey       phase0.BLSPubKey
 }
 
 func (f ValidatorLastStatus) Type() ModelType {
