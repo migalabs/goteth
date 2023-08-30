75725d6af85948a4bd8e267a518ddbb95e87fd6a
Tarun <tarsuno@gmail.com>
2023-08-30 13:51:08 +0200

    Reduce metrics to only critical

--- pkg/db/metrics.go
+++ pkg/db/metrics.go
@@ -6,18 +6,10 @@ import (
 )
 
 type DBMetrics struct {
-	Block               bool
-	Epoch               bool
-	PoolSummary         bool
-	ProposerDuties      bool
-	ValidatorLastStatus bool
-	ValidatorRewards    bool
-	Withdrawals         bool
-	Transactions        bool
-
-	BlockDownload   bool
-	StateDownload   bool
-	RewardsDownload bool
+	Block            bool
+	Epoch            bool
+	ValidatorRewards bool
+	Transactions     bool
 }
 
 func NewMetrics(input string) (DBMetrics, error) {
@@ -28,25 +20,12 @@ func NewMetrics(input string) (DBMetrics, error) {
 		switch item {
 		case "block":
 			dbMetrics.Block = true
-			dbMetrics.BlockDownload = true
 		case "epoch":
 			dbMetrics.Epoch = true
-			dbMetrics.StateDownload = true
-		case "pool_summary":
-			dbMetrics.PoolSummary = true
-		case "validator_last_status":
-			dbMetrics.ValidatorLastStatus = true
-			dbMetrics.StateDownload = true
-		case "validator":
+		case "rewards":
 			dbMetrics.ValidatorRewards = true
-			dbMetrics.StateDownload = true
-			dbMetrics.RewardsDownload = true
-		case "withdrawals":
-			dbMetrics.Withdrawals = true
-			dbMetrics.BlockDownload = true
 		case "transactions":
 			dbMetrics.Transactions = true
-			dbMetrics.BlockDownload = true
 		default:
 			return DBMetrics{}, fmt.Errorf("could not parse metric: %s", item)
 		}
