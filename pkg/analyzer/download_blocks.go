302d1eec2f36ad42243dd8f3a4b17a0551738fd0
Tarun <tarsuno@gmail.com>
2023-09-08 10:50:26 +0200

    Only store orphans which were proposed

--- pkg/analyzer/download_blocks.go
+++ pkg/analyzer/download_blocks.go
@@ -236,9 +236,9 @@ func (s *ChainAnalyzer) Reorg(baseSlot phase0.Slot, slot phase0.Slot, queue *Sta
 	var orphanBlock db.OrphanBlock
 	for i := baseSlot; i < slot; i++ {
 
-		_, ok := queue.BlockHistory[i]
+		block, ok := queue.BlockHistory[i]
 
-		if ok { // only persist orphans if we had downloaded them
+		if ok && block.Proposed { // only persist orphans if we had downloaded them and they were proposed
 			orphanBlock = db.OrphanBlock(queue.BlockHistory[i])
 			s.dbClient.Persist(orphanBlock)
 		}
