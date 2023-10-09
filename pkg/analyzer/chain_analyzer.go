1d2ca063e4b47d088d2e2dd24aea7e31ea76049f
Tarun <tarsuno@gmail.com>
2023-10-09 08:35:07 +0200

    First commit to initiate whole refactor

---
+++ pkg/analyzer/chain_analyzer.go
@@ -0,0 +1,163 @@
+package analyzer
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/migalabs/goteth/pkg/clientapi"
+	"github.com/migalabs/goteth/pkg/config"
+	"github.com/migalabs/goteth/pkg/db"
+	prom_metrics "github.com/migalabs/goteth/pkg/metrics"
+	"github.com/migalabs/goteth/pkg/utils"
+
+	"github.com/migalabs/goteth/pkg/events"
+	"github.com/pkg/errors"
+)
+
+type ChainAnalyzer struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	// Slot Range
+	initSlot  phase0.Slot
+	finalSlot phase0.Slot
+
+	// Channels
+	downloadTaskChan chan phase0.Slot
+
+	// Connections
+	cli       *clientapi.APIClient
+	eventsObj events.Events
+	dbClient  *db.PostgresDBService
+
+	// Control Variables
+	stop               bool
+	downloadFinished   bool
+	processerFinished  bool
+	routineClosed      chan struct{}
+	downloadMode       string
+	validatorWorkerNum int
+	metrics            db.DBMetrics
+	processerBook      *utils.RoutineBook
+
+	queue Queue
+
+	initTime    time.Time
+	PromMetrics *prom_metrics.PrometheusMetrics
+}
+
+func NewChainAnalyzer(
+	pCtx context.Context,
+	iConfig config.AnalyzerConfig) (*ChainAnalyzer, error) {
+
+	// gen new ctx from parent
+	ctx, cancel := context.WithCancel(pCtx)
+
+	// calculate the list of slots that we will analyze
+
+	if iConfig.DownloadMode == "hybrid" || iConfig.DownloadMode == "historical" {
+
+		if iConfig.FinalSlot <= iConfig.InitSlot {
+			return &ChainAnalyzer{}, errors.Errorf("Final Slot cannot be greater than Init Slot")
+		}
+
+		log.Infof("generating new Block Analyzer from slots %d:%d", iConfig.InitSlot, iConfig.FinalSlot)
+	}
+
+	metricsObj, err := db.NewMetrics(iConfig.Metrics)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to read metric.")
+	}
+
+	idbClient, err := db.New(ctx, iConfig.DBUrl, db.WithWorkers(iConfig.DbWorkerNum))
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to generate DB Client.")
+	}
+
+	idbClient.Connect()
+
+	// generate the httpAPI client
+	cli, err := clientapi.NewAPIClient(pCtx,
+		iConfig.BnEndpoint,
+		clientapi.WithELEndpoint(iConfig.ElEndpoint),
+		clientapi.WithDBMetrics(metricsObj))
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to generate API Client.")
+	}
+
+	// generate the central exporting service
+	promethMetrics := prom_metrics.NewPrometheusMetrics(ctx, "0.0.0.0", iConfig.PrometheusPort)
+
+	analyzer := &ChainAnalyzer{
+		ctx:                ctx,
+		cancel:             cancel,
+		initSlot:           phase0.Slot(iConfig.InitSlot),
+		finalSlot:          phase0.Slot(iConfig.FinalSlot),
+		downloadTaskChan:   make(chan phase0.Slot),
+		validatorWorkerNum: iConfig.WorkerNum,
+		cli:                cli,
+		dbClient:           idbClient,
+		routineClosed:      make(chan struct{}, 1),
+		eventsObj:          events.NewEventsObj(ctx, cli),
+		downloadMode:       iConfig.DownloadMode,
+		metrics:            metricsObj,
+		PromMetrics:        promethMetrics,
+		queue:              NewQueue(),
+		processerBook:      utils.NewRoutineBook(100),
+	}
+
+	InitGenesis(analyzer.dbClient, analyzer.cli)
+
+	analyzerMet := analyzer.GetPrometheusMetrics()
+	promethMetrics.AddMeticsModule(analyzerMet)
+
+	return analyzer, nil
+}
+
+func (s *ChainAnalyzer) Run() {
+	defer s.cancel()
+	// Get init time
+	s.initTime = time.Now()
+
+	log.Info("Blocks Analyzer initialized at ", s.initTime)
+
+	// Block requester
+	var wgDownload sync.WaitGroup
+
+	totalTime := int64(0)
+	start := time.Now()
+
+	wgDownload.Add(1)
+	go s.runDownloadBlocks(&wgDownload)
+	if s.downloadMode == "historical" {
+		// Block requester + Task generator
+		wgDownload.Add(1)
+		go s.runHistorical()
+	}
+
+	if s.downloadMode == "finalized" {
+		// Block requester in finalized slots, not used for now
+		wgDownload.Add(1)
+		go s.runHead()
+	}
+
+	s.PromMetrics.Start()
+
+	wgDownload.Wait()
+	s.downloadFinished = true
+
+	s.dbClient.Finish()
+
+	totalTime += int64(time.Since(start).Seconds())
+	analysisDuration := time.Since(s.initTime).Seconds()
+	log.Info("Blocks Analyzer finished in ", analysisDuration)
+	s.routineClosed <- struct{}{}
+}
+
+func (s *ChainAnalyzer) Close() {
+	log.Info("Sudden closed detected, closing StateAnalyzer")
+	s.stop = true
+	<-s.routineClosed // Wait for services to stop before returning
+}
