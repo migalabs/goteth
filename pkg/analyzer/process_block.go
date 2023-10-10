ee90897c03464a28652ed1929dab1c5ac1e91377
Tarun <tarsuno@gmail.com>
2023-10-10 10:08:01 +0200

    Arguments through goroutine

--- pkg/analyzer/process_block.go
+++ pkg/analyzer/process_block.go
@@ -3,6 +3,7 @@ package analyzer
 import (
 	"fmt"
 
+	"github.com/attestantio/go-eth2-client/spec/bellatrix"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
 	"github.com/migalabs/goteth/pkg/spec"
 )
@@ -25,19 +26,19 @@ func (s *ChainAnalyzer) ProcessBlock(slot phase0.Slot) {
 
 func (s ChainAnalyzer) processTransactions(block spec.AgnosticBlock) {
 
-	for idx, transaction := range block.ExecutionPayload.Transactions {
-		go func() {
+	for idx, tx := range block.ExecutionPayload.Transactions {
+		go func(txID int, transaction bellatrix.Transaction) {
 			detailedTx, err := s.cli.RequestTransactionDetails(
 				transaction,
 				block.Slot,
 				block.ExecutionPayload.BlockNumber,
 				block.ExecutionPayload.Timestamp)
 			if err != nil {
-				log.Errorf("could not request transaction details in slot %s for transaction %d: %s", block.Slot, idx, err)
+				log.Errorf("could not request transaction details in slot %s for transaction %d: %s", block.Slot, txID, err)
 			}
-			log.Tracef("persisting transaction metrics: slot %d, tx number: %d", block.Slot, idx)
+			log.Tracef("persisting transaction metrics: slot %d, tx number: %d", block.Slot, txID)
 			s.dbClient.Persist(detailedTx)
-		}()
+		}(idx, tx)
 
 	}
 }
