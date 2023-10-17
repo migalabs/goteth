5a503af5c33ab561299ef51c264b29e0ddfb699a
Tarun <tarsuno@gmail.com>
2023-10-17 12:46:59 +0200

    Methods update the queue

--- pkg/analyzer/download.go
+++ pkg/analyzer/download.go
@@ -4,7 +4,7 @@ import (
 	"github.com/attestantio/go-eth2-client/spec/phase0"
 )
 
-func (s ChainAnalyzer) DownloadBlock(slot phase0.Slot) {
+func (s *ChainAnalyzer) DownloadBlock(slot phase0.Slot) {
 	if !s.metrics.Block {
 		log.Infof("skipping block download at slot %d: no metrics activated for block...", slot)
 		return
@@ -19,7 +19,7 @@ func (s ChainAnalyzer) DownloadBlock(slot phase0.Slot) {
 	// check if the min Request time has been completed (to avoid spaming the API)
 }
 
-func (s ChainAnalyzer) DownloadState(slot phase0.Slot) {
+func (s *ChainAnalyzer) DownloadState(slot phase0.Slot) {
 	if !s.metrics.Epoch {
 		log.Infof("skipping state download: no metrics activated for state...")
 		return
