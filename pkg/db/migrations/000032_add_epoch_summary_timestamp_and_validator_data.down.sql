f8fbccd11f59d39dd8498da23f83210eeb52d942
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2023-11-27 13:16:35 -0300

    remove function and trigger

--- pkg/db/migrations/000032_add_epoch_summary_timestamp_and_validator_data.down.sql
+++ pkg/db/migrations/000032_add_epoch_summary_timestamp_and_validator_data.down.sql
@@ -1,9 +1,3 @@
--- Drop trigger
-DROP TRIGGER set_default_timestamp ON t_epoch_metrics_summary;
-
--- Drop function
-DROP FUNCTION calculate_default_timestamp();
-
 ALTER TABLE t_epoch_metrics_summary DROP COLUMN f_timestamp;
 
 ALTER TABLE t_epoch_metrics_summary DROP COLUMN f_num_slashed;
--- pkg/db/migrations/000032_add_epoch_summary_timestamp_and_validator_data.up.sql
+++ pkg/db/migrations/000032_add_epoch_summary_timestamp_and_validator_data.up.sql
@@ -1,21 +1,5 @@
 ALTER TABLE t_epoch_metrics_summary ADD COLUMN f_timestamp BIGINT;
 
-CREATE OR REPLACE FUNCTION calculate_default_timestamp()
-  RETURNS TRIGGER AS
-$$
-BEGIN
-  NEW.f_timestamp := (SELECT f_genesis_time FROM t_genesis LIMIT 1) + (NEW.f_epoch * 32 * 12);
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-
-
-CREATE TRIGGER set_default_timestamp
-  BEFORE INSERT ON t_epoch_metrics_summary
-  FOR EACH ROW
-  EXECUTE FUNCTION calculate_default_timestamp();
-
 UPDATE t_epoch_metrics_summary
 SET f_timestamp = (SELECT f_genesis_time FROM t_genesis LIMIT 1) + (f_epoch * 32 * 12);
 
