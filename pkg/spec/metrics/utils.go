a81eacee87fd7a2e9576afc90574aecaecd0fb7e
Tarun <tarsuno@gmail.com>
2023-12-01 13:24:16 +0100

    Remove Targt Function and add get justified root function

--- pkg/spec/metrics/utils.go
+++ pkg/spec/metrics/utils.go
@@ -33,25 +33,25 @@ func (p AltairMetrics) GetValidatorFromCommitteeIndex(slot phase0.Slot, committe
 	return 0, fmt.Errorf("could not get validator from any epoch: slot %d, committee %d, index %d", slot, committeeIndex, idx)
 }
 
-func (p AltairMetrics) GetTargetBlockfromSlot(slot phase0.Slot) (spec.AgnosticBlock, error) {
+func (p AltairMetrics) GetJustifiedRootfromSlot(slot phase0.Slot) (phase0.Root, error) {
 	if slot >= phase0.Slot(p.baseMetrics.PrevState.Epoch)*spec.SlotsPerEpoch &&
 		slot < phase0.Slot(p.baseMetrics.CurrentState.Epoch)*spec.SlotsPerEpoch {
 		// slot in PrevEpoch
-		return p.baseMetrics.PrevState.Blocks[0], nil
+		return p.baseMetrics.PrevState.CurrentJustifiedCheckpoint.Root, nil
 	}
 
 	if slot >= phase0.Slot(p.baseMetrics.CurrentState.Epoch)*spec.SlotsPerEpoch &&
 		slot < phase0.Slot(p.baseMetrics.NextState.Epoch)*spec.SlotsPerEpoch {
 		// slot in CurrentEpochEpoch
-		return p.baseMetrics.CurrentState.Blocks[0], nil
+		return p.baseMetrics.CurrentState.CurrentJustifiedCheckpoint.Root, nil
 	}
 
 	if slot >= phase0.Slot(p.baseMetrics.NextState.Epoch)*spec.SlotsPerEpoch &&
 		slot < phase0.Slot(p.baseMetrics.NextState.Epoch+1)*spec.SlotsPerEpoch {
 		// slot in NextEpoch
-		return p.baseMetrics.NextState.Blocks[0], nil
+		return p.baseMetrics.NextState.CurrentJustifiedCheckpoint.Root, nil
 	}
 
-	return spec.AgnosticBlock{}, errors.New("could not get validator from any epoch")
+	return phase0.Root{}, errors.New("could not get justified checkpoint from any epoch")
 
 }
