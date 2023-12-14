41636829d74f05b5f1b1781c4d26ff918fe3caf4
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2023-12-14 13:41:58 -0300

    add test

---
+++ tests/db_head_events_test.py
@@ -0,0 +1,22 @@
+import unittest
+import unit_db_test.testcase as dbtest
+
+class CheckIntegrityOfDB(dbtest.DBintegrityTest):
+    db_config_file = ".env"
+
+    def test_arrival_timestamp_not_gt_block_timestamp(self):
+        """ On the edge case where the arrival timestamp is not greater than the block timestamp, which is not possible """
+        sql_query = """
+        SELECT *
+        FROM t_head_events
+        JOIN t_block_metrics ON t_head_events.f_slot = t_block_metrics.f_slot
+        WHERE t_head_events.f_arrival_timestamp < t_block_metrics.f_timestamp;
+
+        """
+        df = self.db.get_df_from_sql_query(sql_query)
+        self.assertNoRows(df)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
