a7e2f4715e773f7d5d206e39d0e42f39aef0e2a3
Tarun <tarsuno@gmail.com>
2024-02-12 13:00:59 +0100

    Fix test and fix bug in proposer api rewards

--- tests/db_validator_test.py
+++ tests/db_validator_test.py
@@ -9,7 +9,7 @@ class CheckIntegrityOfDB(dbtest.DBintegrityTest):
         sql_query = """
         select *
         from t_validator_rewards_summary
-        where (f_status = 1 or f_status = 3) and abs(f_reward) > abs(f_max_reward)
+        where f_status = 1 and abs(f_reward) > abs(f_max_reward)
         """
         df = self.db.get_df_from_sql_query(sql_query)
         self.assertNoRows(df)
@@ -17,9 +17,9 @@ class CheckIntegrityOfDB(dbtest.DBintegrityTest):
     def test_unpersisted_queries_for_validator_duties(self):
         """ There shouldn't be less proposer duties than the 32 slots per epoch, if so, there were some DB queries lost on the way """
         sql_query = """
-        select f_proposer_slot/32 as epoch, count(*)
+        select trunc(f_proposer_slot/32) as epoch, count(*)
         from t_proposer_duties
-        group by f_proposer_slot/32
+        group by trunc(f_proposer_slot/32)
         having count(*) < 32
         """
         df = self.db.get_df_from_sql_query(sql_query)
@@ -28,9 +28,9 @@ class CheckIntegrityOfDB(dbtest.DBintegrityTest):
     def test_weird_error_tracking_validator_duties(self):
         """ We could expect less that 32 duties/epoch because some queries were lost, but if we have more, there is something weird going on """
         sql_query = """
-        select f_proposer_slot/32 as epoch, count(*)
+        select trunc(f_proposer_slot/32) as epoch, count(*)
         from t_proposer_duties
-        group by f_proposer_slot/32
+        group by trunc(f_proposer_slot/32)
         having count(*) > 32
         """
         df = self.db.get_df_from_sql_query(sql_query)
