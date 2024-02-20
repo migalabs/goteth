4687216e05fde17e25ad69787661414bef4cf2c8
Tarun <tarsuno@gmail.com>
2024-02-20 15:34:34 +0100

    Adjust migrations to Clickhouse after rebase

---
+++ pkg/db/migrations/000002_add_inclusion_delay_column.up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE t_validator_rewards_summary ADD COLUMN f_inclusion_delay UInt8 DEFAULT 0;
--- pkg/db/migrations/000035_add_inclusion_delay_column.up.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE t_validator_rewards_summary ADD COLUMN f_inclusion_delay SMALLINT NOT NULL DEFAULT 0;
