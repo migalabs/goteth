7342a9326b155462569ee1f788502d9aaaf4ce0b
Tarun <tarsuno@gmail.com>
2024-04-11 14:25:44 +0200

    Add test

--- tests/db_epoch_metrics_test.py
+++ tests/db_epoch_metrics_test.py
@@ -13,6 +13,21 @@ class CheckIntegrityOfDB(dbtest.DBintegrityTest):
         """
         df = self.db.get_df_from_sql_query(sql_query)
         self.assertNoRows(df)
+    
+    def test_att_balance_less_than_total(self):
+        """ Some attesting balance is greater than the total effective balance in the epoch"""
+        sql_query = """
+        SELECT *
+        FROM t_epoch_metrics_summary
+        WHERE (
+            	f_att_effective_balance_eth > f_total_effective_balance_eth or
+				f_source_att_effective_balance_eth > f_total_effective_balance_eth or 
+                f_target_att_effective_balance_eth > f_total_effective_balance_eth or
+                f_head_att_effective_balance_eth > f_total_effective_balance_eth
+        )
+        """
+        df = self.db.get_df_from_sql_query(sql_query)
+        self.assertNoRows(df)
 
 if __name__ == '__main__':
     unittest.main()
