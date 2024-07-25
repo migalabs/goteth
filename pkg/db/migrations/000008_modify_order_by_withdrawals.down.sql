b9f06d16adec10f87c4096816143487448078fbd
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2024-07-25 16:45:19 -0300

    change t_withdrawals to order by index

---
+++ pkg/db/migrations/000008_modify_order_by_withdrawals.down.sql
@@ -0,0 +1,20 @@
+-- Create new table with desired order by
+CREATE TABLE new_t_withdrawals(
+	f_slot UInt64,
+	f_index UInt64,
+	f_val_idx UInt64,
+	f_address TEXT,
+	f_amount UInt64)
+	ENGINE = ReplacingMergeTree()
+	ORDER BY (f_slot);
+
+-- Populate new table
+INSERT INTO new_t_withdrawals
+SELECT * FROM t_withdrawals;
+
+-- Rename tables
+RENAME TABLE t_withdrawals TO t_withdrawals_old;
+RENAME TABLE new_t_withdrawals TO t_withdrawals;
+
+-- Drop temporary table
+DROP TABLE t_withdrawals_old;
