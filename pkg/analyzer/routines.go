0711381f419a980c64403304f7a4de19e76bda04
“santi1234567” <45318759+santi1234567@users.noreply.github.com>
2024-09-24 10:41:18 -0300

    always download two epochs before the starting point

--- pkg/analyzer/routines.go
+++ pkg/analyzer/routines.go
@@ -131,15 +131,15 @@ func (s *ChainAnalyzer) fillToHead() phase0.Slot {
 		log.Errorf("could not obtain last slot in database: %s", err)
 	}
 	// if we did not get a last slot from the database, or we were too close to the head
-	// then start from the current finalized in the chain
+	// then start from two epochs before current finalized in the chain
 	if nextSlotDownload == 0 || nextSlotDownload > finalizedBlock.Slot {
 		log.Infof("continue from finalized slot %d, epoch %d", finalizedBlock.Slot, finalizedBlock.Slot/spec.SlotsPerEpoch)
-		nextSlotDownload = finalizedBlock.Slot
+		nextSlotDownload = finalizedBlock.Slot - (epochsToFinalizedTentative * spec.SlotsPerEpoch) // 2 epochs before
+
 	} else {
 		// database detected
 		log.Infof("database detected, continue from slot %d, epoch %d", nextSlotDownload, nextSlotDownload/spec.SlotsPerEpoch)
 		nextSlotDownload = nextSlotDownload - (epochsToFinalizedTentative * spec.SlotsPerEpoch) // 2 epochs before
-
 	}
 	nextSlotDownload = nextSlotDownload / spec.SlotsPerEpoch * spec.SlotsPerEpoch
 	s.initSlot = nextSlotDownload / spec.SlotsPerEpoch * spec.SlotsPerEpoch
