44108236977cee85f7657f749040a4d860a3cffb
“santi1234567” <45318759+santi1234567@users.noreply.github.com>
2025-01-31 12:02:24 -0300

    implement attestations included in pool summaries

--- pkg/db/migrations/000018_add_att_included_val_rewards_table.down.sql
+++ pkg/db/migrations/000018_add_att_included_val_rewards_table.down.sql
@@ -2,3 +2,5 @@ ALTER TABLE t_validator_rewards_summary DROP COLUMN f_attestation_included;
 
 
 ALTER TABLE t_validator_rewards_aggregation DROP COLUMN f_attestations_included;
+
+ALTER TABLE t_pool_summary DROP COLUMN count_attestations_included;
