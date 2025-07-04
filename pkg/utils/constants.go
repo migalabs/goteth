1567ebf848c745158f1a3ed678a1c9d573720fea
“santi1234567” <45318759+santi1234567@users.noreply.github.com>
2025-07-04 16:32:06 -0300

    fix: update version to v3.6.6

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -3,7 +3,7 @@ package utils
 import "time"
 
 const (
-	Version                = "v3.6.5"
+	Version                = "v3.6.6"
 	CliName                = "GotEth"
 	RoutineFlushTimeout    = time.Duration(1 * time.Second)
 	AcquireWaitIntervalLog = 1 * time.Minute
