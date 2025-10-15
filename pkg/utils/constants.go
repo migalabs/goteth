296b72adb3a08760f88c609dbc05e9bc676a4165
“santi1234567” <45318759+santi1234567@users.noreply.github.com>
2025-10-15 16:39:20 -0300

    Bump version to v3.7.0

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -3,7 +3,7 @@ package utils
 import "time"
 
 const (
-	Version                = "v3.6.8"
+	Version                = "v3.7.0"
 	CliName                = "GotEth"
 	RoutineFlushTimeout    = time.Duration(1 * time.Second)
 	AcquireWaitIntervalLog = 1 * time.Minute
